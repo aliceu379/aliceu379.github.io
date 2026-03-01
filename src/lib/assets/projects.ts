9ef761f7b87f1680f0e3e8e7128904d44f7aa9a8
RothCollin <rothcu621@protonmail.com>
2026-03-01 19:15:48 +0700

    feat: add 16bit-computer project

--- src/lib/assets/projects.ts
+++ src/lib/assets/projects.ts
@@ -8,6 +8,14 @@ interface Project {
 	description: string
 }
 
+function linkImage(imgName: string): string {
+	return "/img/projects/".concat(imgName)
+}
+
+function linkCodeberg(repoName: string): string {
+	return "https://codeberg.org/RothCu/".concat(repoName)
+}
+
 export function normalizeProject(project: Partial<Project>): Project {
 	return {
 		name: project.name ?? "Unnamed Project",
@@ -22,6 +30,10 @@ export function normalizeProject(project: Partial<Project>): Project {
 
 export const projectInformation: Array<Partial<Project>> = [
 	{
-		name: "Test",
+		name: "16 Bit Computer",
+		description: "A simple 16 bit computer made in Logisim Evolution, capable of doing arithmetics and conditionals",
+		href: linkCodeberg("16bit-computer"),
+		img: linkImage("16bit_computer.png"),
+		alt: "A circuit of the 16 bit computer",
 	},
 ];
